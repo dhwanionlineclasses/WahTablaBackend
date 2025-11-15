24a27e8d88d97e9829b8ccc2536215d96d6087b6
thesarinjaizz09 <jazzsarin28@gmail.com>
2025-11-15 15:35:40 +0530

    feat: implemented razorpay

--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import bodyParser from "body-parser";
-import { createCheckoutSession, handleStripeWebhook }  from "../controllers/payment.controller";
+import { createCheckoutSession, createOrderId, handleStripeWebhook, verifyPayment } from "../controllers/payment.controller";
 
 const router = Router();
 
-router.post('/createCheckout',  createCheckoutSession);
+router.post('/createCheckout', createCheckoutSession);
 
-router.post('/webhook', bodyParser.raw({type: 'application/json'}),handleStripeWebhook);
+router.post('/webhook', bodyParser.raw({ type: 'application/json' }), handleStripeWebhook);
 
+router.post('/createOrderId', createOrderId);
 
+router.post('/verifyPayment', bodyParser.raw({ type: 'application/json' }), verifyPayment);
 
 export default router;
